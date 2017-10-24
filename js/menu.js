cff8368dc29d605b8338da0292bd1331bdf7fdf4
Jordan Burns <Jordan.Burns@atriacom.com>
2017-10-24 11:26:53 -0400

    added reference for googlesheet DB in comments

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,11 @@
+/* 
+Google Spreadsheet Menu 
+json
+    https://spreadsheets.google.com/feeds/list/1dSSJioqfGgVI2tW7AKWOWt4ZjuRwOqf7pSkcKIZlCm8/1/public/basic?alt=json
+csv 
+    https://docs.google.com/spreadsheets/d/e/2PACX-1vQCcIj98RPRZJmnGqBb8d0pwpyUhq_PUTB6R6UBhVoibQSEK97nnleap5LV4C6n_K8NUBHGU90tvr8K/pub?gid=0&single=true&output=csv
+*/
+
 var source = $("#some-template").html(); 
 var template = Handlebars.compile(source); 
 
