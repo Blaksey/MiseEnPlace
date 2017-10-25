7e59b8e990d183a8ce3ac11e2b8b7dffd9110628
Jordan Burns <Jordan.Burns@atriacom.com>
2017-10-25 14:05:26 -0400

    working off live json, looking to further improve json feed in for flexibiltiy

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@
 Google Spreadsheet Menu 
 json
     https://spreadsheets.google.com/feeds/list/1dSSJioqfGgVI2tW7AKWOWt4ZjuRwOqf7pSkcKIZlCm8/1/public/basic?alt=json
+    https://spreadsheets.google.com/feeds/cells/1dSSJioqfGgVI2tW7AKWOWt4ZjuRwOqf7pSkcKIZlCm8/1/public/basic?alt=json
 csv 
     https://docs.google.com/spreadsheets/d/e/2PACX-1vQCcIj98RPRZJmnGqBb8d0pwpyUhq_PUTB6R6UBhVoibQSEK97nnleap5LV4C6n_K8NUBHGU90tvr8K/pub?gid=0&single=true&output=csv
 */
@@ -9,63 +10,48 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1dSSJioqfGgVI2tW7AKWOWt4Zj
 
     var Source = $("#foodTemplate").html();
     var Template = Handlebars.compile(Source);
-
+    
     var arr = data.feed.entry;
+    var content = arr[0].content.$t;
+    var split; 
+    var values = [];
+    var calorieData = {};
+    calorieData.Weeks = [];
+    var vegetarianData = {};
+    vegetarianData.Weeks = [];
+
+    //calorieData.Weeks.push({Dates:"Week1", Meal1: "Item1",Meal2: "Item2",Meal3: "Item3",Meal4: "Item4",Meal5: "Item5"});
+    
+
+    
+    for(i in arr) {
+
+        content = arr[i].content.$t;
+        split = content.split(",");
+        values = [];
+
+        for(j in split) {
+            values[j] = split[j].replace(/^[^:]*:\s*/, "");
+            //console.log("Array index: " + j + " Value = " + values[j]);
+        }
 
 
-    //lots of math incoming
-    //now that I have an array containing the data I need to do conditional loops
-    //first check the 'title' for calorie if so add to calorieData else add Vegedata
-    //loop until array.length maxed out
-    var calorieData = {
-        Weeks: [{
-            Dates: "October 23rd-27th",
-            Meal1: "Jerk Chicken w/ Mac & Cheese",
-            Meal2: "Ground Turkey Sweet Potato Casserole w/ Peas",
-            Meal3: "Ground Turkey Sweet Potato Casserole w/ Peas",
-            Meal4: "Lime Pork Carnitas w/ Street Corn",
-            Meal5: "Moroccan Chicken w/ Rice, Chickpeas & Broccoli"
-        }, {
-            Dates: "October 30th-November 3rd",
-            Meal1: "Turkey Chili Stuffed Sweet Potato",
-            Meal2: "Orange Chicken w/ Wheat Rice Noodles & Broccoli",
-            Meal3: "Chicken Verde Enchiladas w/ Black Beans",
-            Meal4: "Ground Turkey Burger w/ BBQ, Lettuce & Roasted Potatoes",
-            Meal5: "Curry Chicken w/ Rice & Peas"
-        }, {
-            Dates: "November 6th-10th",
-            Meal1: "Ground Turkey Marinara Pasta w/ Zucchini",
-            Meal2: "Italian Steak w/ Roasted Chimichurri Cauliflower",
-            Meal3: "Asian Lettuce Wrap w/ Rice",
-            Meal4: "Buffalo Chicken Strips w/ Sweet Potatoes & Broccoli",
-            Meal5: "Chicken, Cheese & Veggie Quesadilla w/ Corn"
-        }]
-    };
+        if(arr[i].title.$t == "Calorie Counted"){
+            //do a push into the weeks array instead of +=
+            calorieData.Weeks.push({Dates:values[0], Meal1: values[1],Meal2: values[2],Meal3:values[3],Meal4: values[4],Meal5: values[5]});
 
-    var vegetarianData = {
-        Weeks: [{
-            Dates: "October 23rd-27th",
-            Meal1: "Jerk Chicken w/ Mac & Cheese",
-            Meal2: "Ground Turkey Sweet Potato Casserole w/ Peas",
-            Meal3: "Ground Turkey Sweet Potato Casserole w/ Peas",
-            Meal4: "Lime Pork Carnitas w/ Street Corn",
-            Meal5: "Moroccan Chicken w/ Rice, Chickpeas & Broccoli"
-        }, {
-            Dates: "October 30th-November 3rd",
-            Meal1: "Turkey Chili Stuffed Sweet Potato",
-            Meal2: "Orange Chicken w/ Wheat Rice Noodles & Broccoli",
-            Meal3: "Chicken Verde Enchiladas w/ Black Beans",
-            Meal4: "Ground Turkey Burger w/ BBQ, Lettuce & Roasted Potatoes",
-            Meal5: "Curry Chicken w/ Rice & Peas"
-        }, {
-            Dates: "November 6th-10th",
-            Meal1: "Ground Turkey Marinara Pasta w/ Zucchini",
-            Meal2: "Italian Steak w/ Roasted Chimichurri Cauliflower",
-            Meal3: "Asian Lettuce Wrap w/ Rice",
-            Meal4: "Buffalo Chicken Strips w/ Sweet Potatoes & Broccoli",
-            Meal5: "Chicken, Cheese & Veggie Quesadilla w/ Corn"
-        }]
-    };
+            
+            //ADD TO CALORIE DATA HERE
+            //console.log("Calorie Counted meal #" + i);
+        } else if(arr[i].title.$t == "Vegetarian") {
+            //ADD TO VEGETARIAN DATA HERE
+            vegetarianData.Weeks.push({Dates:values[0], Meal1: values[1],Meal2: values[2],Meal3:values[3],Meal4: values[4],Meal5: values[5]});
+        } else {
+            console.log("Unsure what this is #" + i);
+            //TOSS IT OUT AND DO NOTHING WITH IT
+        }
+    }
+
 
     $("#calorieMeals").append(Template(calorieData));
     $("#vegetarianMeals").append(Template(vegetarianData));
